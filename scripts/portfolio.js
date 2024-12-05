9076c5cef3d3dd204074c16d5a34e149896fd272
luisjacintom <luis.jacintom@my.unitec.edu.mx>
2024-12-04 21:00:11 -0600

    Actualización de enlaces y corrección de barra de navegación22

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -4,3 +4,18 @@ document.getElementById('contact-form').addEventListener('submit', (e) => {
     alert('¡Gracias por tu mensaje! Me pondré en contacto pronto.');
     e.target.reset();
 });
+//efecto de deslizar
+document.querySelectorAll('#navbar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 70, // Ajusta el desplazamiento si tienes un menú fijo
+                behavior: 'smooth'
+            });
+        }
+    });
+});
